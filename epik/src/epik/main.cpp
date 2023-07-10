438b04d5203040e11dccf7d6a7cd3b202b4b6ddc
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2023-07-10 17:15:04 +0200

    Improved CLI parsing with cxxopts, all options are now available through CLI

--- epik/src/epik/main.cpp
+++ epik/src/epik/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iomanip>
 #include <boost/filesystem.hpp>
+#include <cxxopts.hpp>
 #include <indicators/cursor_control.hpp>
 #include <indicators/progress_bar.hpp>
 #include <i2l/phylo_kmer_db.h>
@@ -30,15 +31,6 @@ std::string make_invocation(int argc, char** argv)
 fs::path make_output_filename(const std::string& input_file, const std::string& output_dir)
 {
     return fs::path(output_dir) / fs::path{ "placements_" + fs::path(input_file).filename().string() + ".jplace" };
-}
-
-void print_line()
-{
-    for (size_t j = 0; j < 60; ++j)
-    {
-        std::cout << '*';
-    }
-    std::cout << std::endl;
 }
 
 template<typename R>
@@ -138,20 +130,44 @@ int main(int argc, char** argv)
 {
     std::ios::sync_with_stdio(false);
 
-    if (argc < 4)
+    cxxopts::Options options(argv[0], "Evolutionary Placement with Informative K-mers");
+    options.add_options()
+        ("d,database", "IPK database", cxxopts::value<std::string>())
+        ("q,query", "Input query file (.fasta)", cxxopts::value<std::string>())
+        ("j,jobs", "Num threads", cxxopts::value<size_t>()->default_value("1"))
+        ("batch-size", "Batch size", cxxopts::value<size_t>()->default_value("2000"))
+        ("omega", "Determines the threshold value", cxxopts::value<float>()->default_value("1.5"))
+        ("mu", "Proportion of the database to load", cxxopts::value<float>()->default_value("1.0"))
+        ("o,output-dir", "Output directory", cxxopts::value<std::string>())
+        ("keep-at-most", "Number of branches to report", cxxopts::value<size_t>()->default_value("7"))
+        ("keep-factor", "Minimum LWR to report", cxxopts::value<double>()->default_value("0.01"))
+        ("h,help", "Print usage")
+        ;
+
+    if (argc == 1)
     {
-        std::cout << "Usage:\n\t" << argv[0] << " DATABASE_FILE OUTPUT_DIRECTORY NUM_THREADS QUERY_FILE" << std::endl;
-        return 1;
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+
+    auto parsed_options = options.parse(argc, argv);
+    if (parsed_options.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        exit(0);
     }
 
     try
     {
-        const auto keep_at_most = size_t{7ul};
-        const auto keep_factor = double{0.01f};
-
-        const auto db_file = std::string{argv[1]};
-        const auto output_dir = std::string{argv[2]};
-        const auto num_threads = std::stoul(argv[3]);
+        const auto db_file = parsed_options["database"].as<std::string>();
+        const auto query_file = parsed_options["query"].as<std::string>();
+        const auto num_threads = parsed_options["jobs"].as<size_t>();
+        const auto batch_size = parsed_options["batch-size"].as<size_t>();
+        const auto user_omega = parsed_options["omega"].as<float>();
+        const auto user_mu = parsed_options["mu"].as<float>();
+        const auto keep_at_most = parsed_options["keep-at-most"].as<size_t>();
+        const auto keep_factor = parsed_options["keep-factor"].as<double>();
+        const auto output_dir = parsed_options["output-dir"].as<std::string>();
 
 #ifndef EPIK_OMP
         if (num_threads != 1)
@@ -161,11 +177,8 @@ int main(int argc, char** argv)
             return -2;
         }
 #endif
-
-        std::cout << "Loading database..." << std::endl;
-        const auto batch_size = 1000u;
-        const auto user_omega = 1.7f;
-        const auto user_mu = 0.5f;
+        std::cout << "Loading database with mu=" << user_mu << " and omega="
+                  << user_omega << "..." << std::endl;
         const auto db = i2l::load(db_file, user_mu, user_omega);
         if (db.version() < i2l::protocol::EARLIEST_INDEX)
         {
@@ -185,95 +198,87 @@ int main(int argc, char** argv)
         auto placer = epik::placer(db, tree, keep_at_most, keep_factor, num_threads);
         /// Here we transform the tree to .newick by our own to make sure the output format is always the same
         const auto tree_as_newick = i2l::io::to_newick(tree, true);
+        const auto jplace_filename = make_output_filename(query_file, output_dir).string();
+        const auto invocation = make_invocation(argc, argv);
+        const auto total_fasta_size = fs::file_size(query_file);
 
-        for (int i = 4; i < argc; ++i)
+        auto jplace = epik::io::jplace_writer(jplace_filename, invocation, tree_as_newick);
+        jplace.start();
+
+        print_intruction_set();
+        std::cout << "Placing " << query_file << "..." << std::endl;
+
+        using namespace indicators;
+        ProgressBar bar{
+            option::BarWidth{60},
+            option::Start{"["},
+            option::Fill{"="},
+            option::Lead{">"},
+            option::Remainder{" "},
+            option::End{"]"},
+            option::PrefixText{"Placing "},
+            option::ForegroundColor{Color::green},
+            option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
+            option::MaxProgress{total_fasta_size}
+        };
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+        size_t num_seq_placed = 0;
+        std::unordered_map<i2l::phylo_kmer::key_type,
+                           std::unordered_map<std::string_view, int>> kmer_map;
+
+        double average_speed = 0.0;
+        size_t num_iterations = 0;
+
+        /// Batch query reading
+        auto reader = i2l::io::batch_fasta(query_file, batch_size);
+        while (true)
         {
-            print_line();
-            const auto query_file = std::string{argv[i]};
-
-            const auto jplace_filename = make_output_filename(query_file, output_dir).string();
-            const auto invocation = make_invocation(argc, argv);
-            const auto total_fasta_size = fs::file_size(query_file);
-
-            auto jplace = epik::io::jplace_writer(jplace_filename, invocation, tree_as_newick);
-            jplace.start();
-
-            std::cout << "Placing " << query_file << "..." << std::endl;
-            print_intruction_set();
-
-            using namespace indicators;
-            ProgressBar bar{
-                option::BarWidth{60},
-                option::Start{"["},
-                option::Fill{"="},
-                option::Lead{">"},
-                option::Remainder{" "},
-                option::End{"]"},
-                option::PrefixText{"Placing "},
-                option::ForegroundColor{Color::green},
-                option::FontStyles{std::vector<FontStyle>{FontStyle::bold}},
-                option::MaxProgress{total_fasta_size}
-            };
-
-            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
-            size_t num_seq_placed = 0;
-            std::unordered_map<i2l::phylo_kmer::key_type,
-                               std::unordered_map<std::string_view, int>> kmer_map;
-
-            double average_speed = 0.0;
-            size_t num_iterations = 0;
-
-            /// Batch query reading
-            auto reader = i2l::io::batch_fasta(query_file, batch_size);
-            while (true)
+            // Synchronous reading of the next batch to place
+            const auto batch = reader.next_batch();
+            if (batch.empty())
             {
-                // Synchronous reading of the next batch to place
-                const auto batch = reader.next_batch();
-                if (batch.empty())
-                {
-                    break;
-                }
-
-                // Place in parallel
-                const auto begin_batch = std::chrono::steady_clock::now();
-                const auto placed_batch = placer.place(batch, num_threads);
-                const auto end_batch = std::chrono::steady_clock::now();
-
-                // Compute placement speed, sequences per second
-                const auto ms_diff = (float)(std::chrono::duration_cast<std::chrono::milliseconds>
-                    (end_batch - begin_batch).count());
-                const auto seq_per_second = 1000.0 * batch_size / ms_diff;
-                average_speed += seq_per_second;
-
-                // Update progress bar
-                bar.set_option(option::PrefixText{humanize(seq_per_second) + " seq/s "});
-                bar.set_option(option::PostfixText{std::to_string(num_seq_placed) + " / ?"});
-                bar.set_progress(reader.bytes_read());
-
-                // Synchronous output to the .jplace file
-                jplace << placed_batch;
-
-                num_seq_placed += batch.size();
-                ++num_iterations;
+                break;
             }
-            jplace.end();
 
-            average_speed /= (double)num_iterations;
-            bar.set_option(option::PrefixText{"Done. "});
-            bar.set_option(option::PostfixText{std::to_string(num_seq_placed)});
+            // Place in parallel
+            const auto begin_batch = std::chrono::steady_clock::now();
+            const auto placed_batch = placer.place(batch, num_threads);
+            const auto end_batch = std::chrono::steady_clock::now();
+
+            // Compute placement speed, sequences per second
+            const auto ms_diff = (float)(std::chrono::duration_cast<std::chrono::milliseconds>
+                (end_batch - begin_batch).count());
+            const auto seq_per_second = 1000.0 * (double)batch_size / ms_diff;
+            average_speed += seq_per_second;
+
+            // Update progress bar
+            bar.set_option(option::PrefixText{humanize(seq_per_second) + " seq/s "});
+            bar.set_option(option::PostfixText{std::to_string(num_seq_placed) + " / ?"});
             bar.set_progress(reader.bytes_read());
 
-            std::cout << std::endl << termcolor::bold << termcolor::white
-                      << "Placed " << num_seq_placed << " sequences.\nAverage speed: "
-                      << humanize(average_speed) << " seq/s.\n";
+            // Synchronous output to the .jplace file
+            jplace << placed_batch;
 
-            const auto placement_time = std::chrono::duration_cast<std::chrono::milliseconds>(
-                std::chrono::steady_clock::now() - begin).count();
-            std::cout << "Placement time: " << humanize_time(placement_time)
-                << " (" << placement_time << " ms)" << termcolor::reset << std::endl;
+            num_seq_placed += batch.size();
+            ++num_iterations;
         }
+        jplace.end();
 
+        average_speed /= (double)num_iterations;
+        bar.set_option(option::PrefixText{"Done. "});
+        bar.set_option(option::PostfixText{std::to_string(num_seq_placed)});
+        bar.set_progress(reader.bytes_read());
+
+        std::cout << std::endl << termcolor::bold << termcolor::white
+                  << "Placed " << num_seq_placed << " sequences.\nAverage speed: "
+                  << humanize(average_speed) << " seq/s.\n";
+
+        const auto placement_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::steady_clock::now() - begin).count();
+        std::cout << "Placement time: " << humanize_time(placement_time)
+            << " (" << placement_time << " ms)" << termcolor::reset << std::endl;
         std::cout << "Done." << '\n' << std::flush;
     }
     catch (const std::runtime_error& error)
